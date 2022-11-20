d92a3db740b27d5170c1faf01b5b691313e0a745
Wildrama <rambo1bc@hotmail.com>
2022-11-19 21:33:18 -0300

    impresion con boton

--- public/imprimirPedido.js
+++ public/imprimirPedido.js
@@ -25,17 +25,20 @@ let pedidosSeleccionados =[{
 }
 ];
 let fechaactual =Date.now().toLocaleString();
-printAllbtn.addEventListener('click', function(){
+function generatePDF(){
+    var pdfObject = jsPDFInvoiceTemplate.default(props); 
+  }
+printAllbtn.addEventListener('click',generatePDF, function(){
     // var doc = new jsPDF();
     // console.log(allCheckBox)
     // // doc.line(10,10,200,200)
-    var pdfObject = jsPDFInvoiceTemplate.default(props);
+    // var pdfObject = jsPDFInvoiceTemplate.default(props);
     
     // var margin = 10;
     // var scale = (doc.internal.pageSize.width - margin *2 )/document.body.scrollWidth;
 
     // pedidosSeleccionados.forEach(pedidoImp =>{
-
+    console.log()
 
     //     //or in browser
 
@@ -53,114 +56,116 @@ printAllbtn.addEventListener('click', function(){
     // if(pedidos.lenght > 1 ){
     // }
     // doc.save(`impresionDeLosPedidos${fechaactual}.pdf`)
+    
 })
-var props = {
-  
-    returnJsPDFDocObject: true,
-    fileName: "Invoice 2021",
-    orientationLandscape: false,
-    compress: true,
-    logo: {
-        src: "https://raw.githubusercontent.com/edisonneza/jspdf-invoice-template/demo/images/logo.png",
-        type: 'PNG', //optional, when src= data:uri (nodejs case)
-        width: 53.33, //aspect ratio = width/height
-        height: 26.66,
-        margin: {
-            top: 0, //negative or positive num, from the current position
-            left: 0 //negative or positive num, from the current position
+
+  var props = {
+outputType: jsPDFInvoiceTemplate.OutputType.Save,
+returnJsPDFDocObject: true,
+fileName: "Invoice 2021",
+orientationLandscape: false,
+compress: true,
+logo: {
+    src: "https://raw.githubusercontent.com/edisonneza/jspdf-invoice-template/demo/images/logo.png",
+    type: 'PNG', //optional, when src= data:uri (nodejs case)
+    width: 53.33, //aspect ratio = width/height
+    height: 26.66,
+    margin: {
+        top: 0, //negative or positive num, from the current position
+        left: 0 //negative or positive num, from the current position
+    }
+},
+stamp: {
+    inAllPages: true, //by default = false, just in the last page
+    src: "https://raw.githubusercontent.com/edisonneza/jspdf-invoice-template/demo/images/qr_code.jpg",
+    type: 'JPG', //optional, when src= data:uri (nodejs case)
+    width: 20, //aspect ratio = width/height
+    height: 20,
+    margin: {
+        top: 0, //negative or positive num, from the current position
+        left: 0 //negative or positive num, from the current position
+    }
+},
+business: {
+    name: "Isidorito",
+    address: "San Pedro, Buenos Aires",
+    phone: "3329",
+    email: "isidorito@example.com",
+    email_1: "info@example.al",
+    website: "www.example.al",
+},
+contact: {
+    label: "Invoice issued for:",
+    name: "Client Name",
+    address: "Albania, Tirane, Astir",
+    phone: "(+355) 069 22 22 222",
+    email: "client@website.al",
+    otherInfo: "www.website.al",
+},
+invoice: {
+    label: "Invoice #: ",
+    num: 19,
+    invDate: "Payment Date: 01/01/2021 18:12",
+    invGenDate: "Invoice Date: 02/02/2021 10:17",
+    headerBorder: false,
+    tableBodyBorder: false,
+    header: [
+      {
+        title: "#", 
+        style: { 
+          width: 10 
+        } 
+      }, 
+      { 
+        title: "Title",
+        style: {
+          width: 30
+        } 
+      }, 
+      { 
+        title: "Description",
+        style: {
+          width: 80
+        } 
+      }, 
+      { title: "Price"},
+      { title: "Quantity"},
+      { title: "Unit"},
+      { title: "Total"}
+    ],
+    table: Array.from(Array(10), (item, index)=>([
+      
+    ])),
+    additionalRows: [{
+        col1: 'Total:',
+        col2: '145,250.50',
+        col3: 'ALL',
+        style: {
+            fontSize: 14 //optional, default 12
         }
     },
-    stamp: {
-        inAllPages: true, //by default = false, just in the last page
-        src: "https://raw.githubusercontent.com/edisonneza/jspdf-invoice-template/demo/images/qr_code.jpg",
-        type: 'JPG', //optional, when src= data:uri (nodejs case)
-        width: 20, //aspect ratio = width/height
-        height: 20,
-        margin: {
-            top: 0, //negative or positive num, from the current position
-            left: 0 //negative or positive num, from the current position
+    {
+        col1: 'VAT:',
+        col2: '20',
+        col3: '%',
+        style: {
+            fontSize: 10 //optional, default 12
         }
     },
-    business: {
-        name: "Business Name",
-        address: "Albania, Tirane ish-Dogana, Durres 2001",
-        phone: "(+355) 069 11 11 111",
-        email: "email@example.com",
-        email_1: "info@example.al",
-        website: "www.example.al",
-    },
-    contact: {
-        label: "Invoice issued for:",
-        name: "Client Name",
-        address: "Albania, Tirane, Astir",
-        phone: "(+355) 069 22 22 222",
-        email: "client@website.al",
-        otherInfo: "www.website.al",
-    },
-    invoice: {
-        label: "Invoice #: ",
-        num: 19,
-        invDate: "Payment Date: 01/01/2021 18:12",
-        invGenDate: "Invoice Date: 02/02/2021 10:17",
-        headerBorder: false,
-        tableBodyBorder: false,
-        header: [
-          {
-            title: "#", 
-            style: { 
-              width: 10 
-            } 
-          }, 
-          { 
-            title: "Title",
-            style: {
-              width: 30
-            } 
-          }, 
-          { 
-            title: "Description",
-            style: {
-              width: 80
-            } 
-          }, 
-          { title: "Price"},
-          { title: "Quantity"},
-          { title: "Unit"},
-          { title: "Total"}
-        ],
-        table: Array.from(Array(10), (item, index)=>([
-        `asdaddas`
-        ])),
-        additionalRows: [{
-            col1: 'Total:',
-            col2: `$aaasasaas}`,
-            col3: 'ALL',
-            style: {
-                fontSize: 14 //optional, default 12
-            }
-        },
-        {
-            col1: 'VAT:',
-            col2: '20',
-            col3: '%',
-            style: {
-                fontSize: 10 //optional, default 12
-            }
-        },
-        {
-            col1: 'SubTotal:',
-            col2: '116,199.90',
-            col3: 'ALL',
-            style: {
-                fontSize: 10 //optional, default 12
-            }
-        }],
-        invDescLabel: "Invoice Note",
-        invDesc: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary.",
-    },
-    footer: {
-        text: "The invoice is created on a computer and is valid without the signature and stamp.",
-    },
-    pageEnable: true,
-    pageLabel: "Page ",
+    {
+        col1: 'SubTotal:',
+        col2: '116,199.90',
+        col3: 'ALL',
+        style: {
+            fontSize: 10 //optional, default 12
+        }
+    }],
+    invDescLabel: "Invoice Note",
+    invDesc: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary.",
+},
+footer: {
+    text: "The invoice is created on a computer and is valid without the signature and stamp.",
+},
+pageEnable: true,
+pageLabel: "Page ",
 };

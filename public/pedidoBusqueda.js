808548156fd26f0ec758a1620164189daefa5172
Wildrama <rambo1bc@hotmail.com>
2022-12-10 05:56:07 -0300

    ui change

--- public/pedidoBusqueda.js
+++ public/pedidoBusqueda.js
@@ -32,7 +32,7 @@ formBuscarPedidos.addEventListener("input", async function (e) {
   const query_buscar = e.target.value;
   displayProductos.innerHTML = "";
   console.log(query_buscar)
-  
+
   try {
     // console.log(query_buscar)
     const res = await axios.get(`/pedidos/buscar-productos?busqueda=${query_buscar}`);
@@ -60,9 +60,9 @@ formBuscarPedidos.addEventListener("input", async function (e) {
       const divPlus = document.createElement('div');
       const idP = document.createElement('p');
 
-      rowProd.classList.add('row', 'mb-1', 'border', 'border-primary', 'py-1')
+      rowProd.classList.add('row', 'mb-1', 'border', 'border-primary', 'p-1')
    
-      col9.classList.add('col-9', 'd-flex', 'justify-content-between', 'text-center')
+      col9.classList.add('col-9', 'd-flex', 'justify-content-between', '')
       col3.classList.add('col-3', 'd-flex', 'justify-content-end', ',align-items-center')
       divCantidadActual.classList.add('px-3')
       idP.classList.add('d-none');
@@ -78,7 +78,7 @@ formBuscarPedidos.addEventListener("input", async function (e) {
       <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
       <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/>
     </svg>`;
-      divCantidadActual.innerHTML = 0,
+      divCantidadActual.innerHTML = "0";
         divPlus.innerHTML = `<svg id="${producto._id}"  xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
       <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
       <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
@@ -102,10 +102,10 @@ formBuscarPedidos.addEventListener("input", async function (e) {
       }
 
       let cantidadElegida = 0;
-   
+      
       console.log(productosAgregadosArr)
       // console.log(idP.textContent)
-     
+      
       let data1 = productosAgregadosArr.find(function (ele) {
         return ele.id === producto._id;
         
@@ -119,13 +119,12 @@ formBuscarPedidos.addEventListener("input", async function (e) {
       }
       divPlus.addEventListener('click', async (e) => {
 
-   
+        
         cantidadElegida += 1;
 
 
 
         if (cantidadElegida < 2) {
-
           // idArray.push(producto._id)
           productosAgregadosArr.push({
             id: producto._id,
